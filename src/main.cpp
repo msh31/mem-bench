9393916524c9e7c75deacd1f1f08146523c57deb
marco <marco@marco007.dev>
2026-01-15 14:56:04 +0100

    feat: add sysinfo support for MacOS

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 #include <cstring>
+#include <string>
+#include <sys/sysctl.h>
+
+#ifdef __linux__
 #include <fstream>
 #include <ostream>
-#include <string>
-#include <sys/resource.h>
+#endif
+
+#ifdef __APPLE__
+#include <mach-o/arch.h>
+#endif
 
 // function implementations are in the named source files
 void run_stack_tests(); 
@@ -14,7 +21,6 @@ void print_system_info() {
 #ifdef __linux__
     std::ifstream cpuModel("/proc/cpuinfo");
     std::ifstream ramCapacity("/proc/meminfo");
-    struct rlimit limit;
 
     std::string cpuLine;
     std::string ramLine;
@@ -37,12 +43,37 @@ void print_system_info() {
             break;
         }
     }
-
-
-    getrlimit(RLIMIT_STACK, &limit);
-    std::cout << "STACK LIMIT: " << limit.rlim_cur / 1024 / 1024 << " MB" << std::endl;
 #elif __APPLE__
+    size_t size{};
+    if (sysctlbyname("machdep.cpu.brand_string", nullptr, &size, nullptr, 0) != 0 || size == 0) {
+        throw std::runtime_error("Unable to retrieve CPU type (size query failed)");
+    }
+
+    std::string buffer(size, '\0');
+    if (sysctlbyname("machdep.cpu.brand_string", buffer.data(), &size, nullptr, 0) != 0) {
+        throw std::runtime_error("Unable to retrieve CPU type (value query failed)");
+    }
+
+    if (!buffer.empty() && buffer.back() == '\0') {
+        buffer.pop_back();
+    }
+
+    int mib [] = { CTL_HW, HW_MEMSIZE };
+    int64_t value = 0;
+    size_t length = sizeof(value);
+
+    if(sysctl(mib, 2, &value, &length, NULL, 0) == -1) {
+        throw std::runtime_error("Unable to retreive total ram amount (sysctl failed)");
+    }
+
+    std::cout << "CPU MODEL: " << buffer << std::endl;
+    std::cout << "TOTAL RAM: " << value / 1024 / 1024 << " MB" << std::endl;
 #endif
+
+//wont work on windows
+    struct rlimit limit;
+    getrlimit(RLIMIT_STACK, &limit);
+    std::cout << "STACK LIMIT: " << limit.rlim_cur / 1024 << " KB" << std::endl;
 }
 
 int main(int argc, char* argv[]) {
